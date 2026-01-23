ad67459f872875d3dd7c218361e9edbff70b0d12
Mario Angarita <mf.angarita52@gmail.com>
2026-01-22 17:03:14 -0900

    changes in sbas

--- volcsarvatory/sbas.py
+++ volcsarvatory/sbas.py
@@ -1,18 +1,20 @@
 import asf_search as asf
+from datetime import datetime, timedelta
 from volcsarvatory import pairs
+from volcsarvatory import prepare_multibursts as pm
 
-def get_first_date(burst_id):
+def first_date_burst(burst_id):
     results = asf.search(fullBurstID=burst_id)
     dates = sorted([r.properties['stopTime'] for r in results if r.properties['stopTime'] is not None])
-    return dates[0]
+    return dates[0].split('T')[0]
 
 
-def get_first_date_multiburst(dic):
+def first_date_multiburst(dic):
     keys = [key for key in dic.keys()]
     burst_id = keys[0]+dic[keys[0]][0]
 
-    return burst_id
-    
+    return first_date_burst(burst_id)
+
 
 def get_season(dic):
     coherence = pairs.get_coherence(dic, num = 1)
@@ -40,3 +42,54 @@ def get_season(dic):
     season = (start, end)
 
     return season, bridge
+
+
+def get_sbas(dic):
+    start = first_date_multiburst(dic)
+    season, bridge = get_season(dic)
+    multiburst = asf.Multiburst(dic)
+    opts = asf.ASFSearchOptions(
+        **{
+            "start": start,
+            "end": datetime.now().strftime("%Y-%m-%d"),
+            "season": pm.get_julian_season(season)
+      }
+    )
+
+    network = asf.Network(
+        multiburst = multiburst,
+        perp_baseline=800, 
+        inseason_temporal_baseline=48,
+        bridge_target_date=bridge.strftime("%m-%d"),
+        bridge_year_threshold=1,
+        opts=opts)
+
+    network.connect_components()
+
+    refs, secs = network.get_multi_burst_pair_ids()
+
+    season = ("1-1", "12-31")
+    opts = asf.ASFSearchOptions(
+        **{
+            "start": datetime.now().strftime("%Y-%m-%d")-timedelta(days = 365),
+            "end": datetime.now().strftime("%Y-%m-%d"),
+            "season": pm.get_julian_season(season)
+      }
+    )
+
+    network = asf.Network(
+        multiburst = multiburst,
+        perp_baseline=800, 
+        inseason_temporal_baseline=144,
+        bridge_target_date=datetime.now().strftime("%m-%d"),
+        bridge_year_threshold=1,
+        opts=opts)
+
+    network.connect_components()
+
+    refs_add, secs_add = network.get_multi_burst_pair_ids()
+
+    refs += refs_add
+    secs += secs_add
+
+    return refs, secs
